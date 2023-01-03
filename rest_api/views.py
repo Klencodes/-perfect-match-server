dd387deae4927ed30d6d52c7f2ef042b636a984d
Josh <klencodes@gmail.com>
2023-01-03 15:39:53 +0000

    Updates

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+# from onesignal_sdk import Client, Notification
+# from decouple import config
 
-# Create your views here.
+# # onesignal_client = Client(app_id=config('ONESIGNAL_APP_ID'), api_key=config('ONESIGNAL_REST_API_KEY'))
+
+# # # Create a notification
+# # notification = Notification(
+# #     contents={
+
+# #         "en": "Hello, world!"
+        
+# #     },
+# #     include_player_ids=["YOUR_USER_ID"],
+# # )
+
+# # # Send the notification
+# # onesignal_client.send_notification(notification)
